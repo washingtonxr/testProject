f639795d488b37785a0a4866abd0cefc69daf259
WASHINGTON RUAN <washingtonxr@live.com>
2021-11-21 10:00:23 +0000

    Append new file.

---
+++ testCpp/cppTestMacPrj/debugModule.hpp
@@ -0,0 +1,18 @@
+//
+//  debugModule.hpp
+//  test1
+//
+//  Created by WASHINGTON RUAN on 11/21/21.
+//
+
+#ifndef debugModule_hpp
+#define debugModule_hpp
+
+#include <stdio.h>
+#include <fstream>
+#define DEBUG_FILE "debug.txt"
+
+int writeFile(float a, float b);
+int readFile(void);
+
+#endif /* debugModule_hpp */
