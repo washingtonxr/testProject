5c58de33da05a2d3236f182c1894a2e357c43cbe
washingtonxr@live.com <Washington Ruan>
2021-11-23 18:52:38 +0800

    Update again.

--- testC/splitTheString/main.c
+++ testC/splitTheString/main.c
@@ -7,27 +7,38 @@ int main(int argc, char *argv)
 {
     char cBuffer[] = "this is washington ruan test code @ 2021";
     const char pDelimiter = ' ';
-#if 0
+    const char pDelimiter2 = 0xa;
+#if 1
     char *pBuffer[MAX_DEPTH] = {0};
     char *pStringSpiltCurrentPrt = NULL;
-    unsigned int ulLoop1 = 0;
+    unsigned char ucLoop1 = 0;
+    unsigned char ucLoop2 = 0;
 
     printf("Sting: %s\n", cBuffer);
-    /*
-    分割同一字符串，第一次调用时传入字符串的首地址，第二个参数是分割符
-    这里的分割符是空格，假如 buf = "hello;world;123"，你还可以按照 ";" 来分割buf
-    此时第二个参数就填写 ";"
-    */
-    pStringSpiltCurrentPrt = strtok(cBuffer, &pDelimiter);
-    while(NULL != pStringSpiltCurrentPrt) {
-        pBuffer[ulLoop1] = pStringSpiltCurrentPrt;
-        ulLoop1++;
-        pStringSpiltCurrentPrt = strtok(NULL, &pDelimiter);
+    printf(">>>>>>>>>>>> cBuffer: %s.\n", cBuffer);
+    for (ucLoop1 = 0; ucLoop1 < strlen(cBuffer); ucLoop1++) {
+        if (pDelimiter == cBuffer[ucLoop1]) {
+            cBuffer[ucLoop1] = pDelimiter2;
+        }
+    }
+    printf(">>>>>>>>>>>> cBuffer: %s.\n", cBuffer);
+    for (ucLoop1 = 0; ucLoop1 < strlen(cBuffer); ucLoop1++) {
+        printf(">>>cBuffer[%d]: 0x%x\n", ucLoop1, cBuffer[ucLoop1]);
     }
 
-    for (ulLoop1 = 0; ulLoop1 < MAX_DEPTH - 1; ulLoop1++) {
-        if (NULL != pBuffer[ulLoop1]) {
-            printf(">>>%s\n", pBuffer[ulLoop1]);
+    pStringSpiltCurrentPrt = strtok(cBuffer, &pDelimiter2);
+    ucLoop1 = 0;
+    while(NULL != pStringSpiltCurrentPrt) {
+        pBuffer[ucLoop1] = pStringSpiltCurrentPrt;
+        printf(">>>ucLoop1:%d - %p - %s\n", ucLoop1, pBuffer[ucLoop1], pBuffer[ucLoop1]);
+        ucLoop1++;
+        pStringSpiltCurrentPrt = strtok(NULL, &pDelimiter2);
+    }
+
+    printf(">>>ucLoop1:%d\n", ucLoop1);
+    for (ucLoop2 = 0; ucLoop2 < ucLoop1; ucLoop2++) {
+        if (NULL != pBuffer[ucLoop2]) {
+            printf(">>>%s\n", pBuffer[ucLoop2]);
         }
     }
 
@@ -42,14 +53,21 @@ int main(int argc, char *argv)
 
     printf(">>>>>>>>>>>> cBuffer: %s.\n", cBuffer);
     for (ucLoop1 = 0; ucLoop1 < strlen(cBuffer); ucLoop1++) {
+        if (pDelimiter == cBuffer[ucLoop1]) {
+            cBuffer[ucLoop1] = pDelimiter2;
+        }
+    }
+
+    printf(">>>>>>>>>>>> cBuffer: %s.\n", cBuffer);
+    for (ucLoop1 = 0; ucLoop1 < strlen(cBuffer); ucLoop1++) {
         printf(">>>cBuffer[%d]: 0x%x\n", ucLoop1, cBuffer[ucLoop1]);
     }
     pStringSpiltHeadPrt = &cBuffer[0];
     for (ucLoop1 = 0; ucLoop1 < strlen(cBuffer) + 1; ucLoop1++) {
         ucAccumulative++;
         pStringSpiltCurrentPrt = &cBuffer[ucLoop1];
-        if (pDelimiter == *pStringSpiltCurrentPrt) {
-            memcpy(pSplitBuffer[ucLoop2], pStringSpiltHeadPrt, ucAccumulative);
+        if (pDelimiter2 == *pStringSpiltCurrentPrt) {
+            memcpy(pSplitBuffer[ucLoop2], pStringSpiltHeadPrt, ucAccumulative - 1);
             printf("%s - %d\n", pSplitBuffer[ucLoop2], ucLoop1);
             pStringSpiltHeadPrt = pStringSpiltCurrentPrt + 1;
             ucLoop2++;
