ffa7de090499b359a711588c780ea79958bbac9b
ruanxi <ruanxi@localhost.localdomain>
2021-12-20 00:53:11 -0500

    Append a ctest project.

---
+++ testCpp/cppTestCmake/test/test.h
@@ -0,0 +1,12 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+
+#include "common.h"
+#include "essentialPack.h"
+#include "algorithmPack.h"
+#include "classPack.h"
+#include "modulePack.h"
+#include "multipleThread.h"
+
+#endif
+/* End of this file. */
